e633b4fa6330ba647a588a4779df32850aa8606d
Julia <117773552+Juliadiasfs@users.noreply.github.com>
2024-02-28 21:59:37 -0300

    Add files via upload

--- revisao/index.php
+++ revisao/index.php
@@ -5,23 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Índice de massa corporal- Revisão</title>
+    <title>Índice de massa corporal - Revisão</title>
 </head>
 <body>
-    <div id="corpo">
-        <form action="imc.php" method="POST">
-            <label>
-            Nome:<input type="text" name="cxnome">
-            </label>
-            <label>
-            Altura:<input type="text" name="cxaltura">
-            </label>
-            <label>
-            Peso:<input type="text" name="cxpeso">
-            </label>
-             <input type="submit" value="IMC">
-     
-         </form>
-   </div>
+
+    <header>
+        <h1>IMC - Índice de Massa Corporal</h1>
+    </header>
+
+    <div id="container">
+        <div id="corpo">
+            <form action="imc.php" method="POST">
+                <label>
+                    Nome:<input type="text" name="cxnome">
+                </label>
+                <label>
+                    Altura:<input type="text" name="cxaltura">
+                </label>
+                <label>
+                    Peso:<input type="text" name="cxpeso" id="peso">
+                </label>
+            
+                <input id="imc" type="submit" value="Calcular IMC">
+            </form>
+        </div>
+    </div>
+
+    <footer>
+        <p>&copy; 2024 Julia Dias. All rights reserved.</p>
+    </footer>
+
 </body>
 </html>
